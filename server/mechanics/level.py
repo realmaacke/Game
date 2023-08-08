d88c42781d4052ba0df69fc247f5886f2eaf994f
Marcus <mackan.pettersson1@outlook.com>
2023-08-09 00:55:31 +0200

    Update level.py

--- server/mechanics/level.py
+++ server/mechanics/level.py
@@ -1,8 +1,11 @@
+import math
+
 
 class Level:
     MinLevel = 1
     MaxLevel = 30
     currentLevel = 1
+    treshhold = 50
 
     MinXP = 0
     MaxXP = 0
@@ -11,9 +14,17 @@ class Level:
     def __init__(self):
         pass
 
+    def percentage(self, part, whole):
+        return 100 * float(part)/float(whole)
 
-    def CalcDiff(self, start, stop):
-        pass
-
-    def levelup(self, level):
-        self.currentXP = 0
+    def difference(self, start, stop):
+        return str(start) - str(stop)
+    
+    def calcLevel(self):
+        requiredXP = math.sqrt(1 - 8 * self.currentLevel * self.treshhold)
+        return requiredXP
+    def levelup(self):
+        if self.currentXP >= self.MaxXP and self.currentLevel < self.MaxLevel:
+            self.currentXP = 0
+        
+    
